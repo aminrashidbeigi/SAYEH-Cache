4adbef5c4da59d8e2055f4d0571bc838c727660a
Amin Rashidbeigi <a.rashidbeigi@gmail.com>
2017-06-17 18:57:18 +0430

    TagValidArray component created

--- DataArray.vhd
+++ DataArray.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity orco is
+entity DataArray is
     port(
     clk: IN std_logic;
     address : IN std_logic_vector(5 downto 0);
@@ -9,9 +9,9 @@ entity orco is
     wrdata : IN std_logic_vector(31 downto 0);
     data : OUT std_logic_vector(31 downto 0)
     );
-end orco;
+end DataArray;
 
-architecture behavioral of orco is
+architecture behavioral of DataArray is
     type date_array is array(0 to 63) of std_logic_vector(31 downto 0);
     signal datas : date_array := (others => "00000000000000000000000000000000");
     begin
---
+++ TagValidArray.vhd
@@ -0,0 +1,30 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity TagValidArray is
+    port(
+    clk: IN std_logic;
+    reset_n: IN std_logic;
+    address : IN std_logic_vector(5 downto 0);
+    wren : IN std_logic;
+    invalidate : IN std_logic;
+    wrdata : IN std_logic_vector(3 downto 0);
+    data : OUT std_logic_vector(4 downto 0)
+    );
+end TagValidArray;
+
+architecture behavioral of TagValidArray is
+    type tag_valid_array is array(0 to 63) of std_logic_vector(4 downto 0);
+    signal tag_valid : tag_valid_array := (others => "00000");
+    begin
+        data <= tag_valid_array(to_integer(unsigned(address)));
+        identifier : process(clk)
+        begin
+            if wren = '1' then
+                tag_valid_array(to_integer(unsigned(address))) <= wrdata;
+            end if;
+            if invalidate = '1' then
+                tag_valid_array(to_integer(unsigned(address)))(4) <= not invalidate;
+            end if;
+        end process;
+end behavioral;
